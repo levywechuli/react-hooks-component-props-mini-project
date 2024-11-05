cf9e6ed69ebf36b22104ebbfed1df9a96b9bcbe8
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-05 09:55:51 +0300

    using skills learnt so far

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About({image, about}){
+    return(
+        <aside>
+            <img src={image || "https://via.placeholder.com/215"} alt="blog logo"></img>
+            <p>{about}</p>
+        </aside>
+
+    );
+
+}
+
+
+
+
+export default About;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+ return(
+       <main>
+        {posts.map((post)=>(
+            <Article key={post.id} title={post.title} preview={post.preview} date={post.date} />
+        ))}
+
+       </main>
+    );
+}
+
+
+export default ArticleList;
